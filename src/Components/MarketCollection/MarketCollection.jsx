ba4f416c5defde4e884c0dd84a0de95077005649
SundarFullStack <meenakshisunder183017@gmail.com>
2024-04-01 17:44:32 -0700

    Carousal commit

--- src/Components/MarketCollection/MarketCollection.jsx
+++ src/Components/MarketCollection/MarketCollection.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./marketCollection.css";
-import Slider from "react-slick";
 import FarmerRices from "../../Images/FarmerRices.png"
 import FarmerCandies from "../../Images/FarmerCandies.png"
 import FarmerDryFruits from "../../Images/FarmerDryFruits.png"
@@ -9,8 +8,8 @@ import FarmerSpices from "../../Images/FarmerSpices.png"
 import FarmerVegetables from "../../Images/FarmerVegetables.png"
 import FarmerFruits from "../../Images/FarmerFruits.png"
 import FarmerFlours from "../../Images/FarmerFlours.png"
-
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 const marketCollection = [
     {
         id: 1,
@@ -64,37 +63,67 @@ const marketCollection = [
   
 const MarketCollection = () => {
 
-    // Slider functionality from "Slick" react component
+ 
 
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1
-      };
-
+       // Carousel responsiveness
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+      slidesToSlide: 1,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 3,
+      slidesToSlide: 1,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
     return (
-     
-        <div className="collection-container">
-      <div className="market-collection">
-          <div className="collection-title">
-              Be Healthy Buy Healthy
+        <div className="home-card-container-cover">
+        <div className="home-card-container">
+          {/* Carousel Container */}
+          <div className="row row-carousel">
+            <Carousel
+              responsive={responsive}
+              removeArrowOnDeviceType={["tablet", "mobile"]}
+            >
+              {/* Product list array mapping */}
+              {marketCollection.map((item, index) => (
+                <div
+                  className="col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-2 home-product-list-container"
+                  key={index + 1}
+                >
+                  {/* Image */}
+                  <div className="home-img-cover home-custom-center pointer">
+                    <img
+                      src={item.url}
+                      className="home-prod_img custom-center"
+                      alt="image"
+                    />
+                  </div>
+                  {/* product name */}
+                  <p className="home-prodName custom-center">
+                    {item.title}
+                  </p>
+               
+                  </div>
+              ))}
+            </Carousel>
           </div>
-          <Slider {...settings}>
-            {marketCollection.map((items) => {
-                return (<div key={items.id}>
-                      <div  className="market-collection-img-cover">
-                      <img src={items.url} className="market-collection-img" />
-                    </div>
-                    <div className="market-collection-img-title">
-                        {items.title}
-                    </div>
-                  </div>)
-              })}
-            </Slider>
-            </div>
-            </div>
+        </div>
+      </div>
+  
       
   )
 }
